c0ff9bdb91f9707da3c3bc246bcd1e3ee0fccafb
段磊 <elele.cool@gmail.com>
2014-01-10 17:41:55 +0800

    新建项目,索引映射

---
+++ bid-search/app/models/bid_item.rb
@@ -0,0 +1,40 @@
+class BidItem < ActiveRecord::Base
+  include Tire::Model::Search
+  include Tire::Model::Callbacks
+
+  # 全文检索
+  mapping do
+    indexes :id, :index => :not_analyzed
+    indexes :title, :analyzer => 'ik'
+    indexes :organizer_id, :index => :not_analyzed
+    indexes :auction_id, :index => :not_analyzed
+    indexes :lot
+    indexes :description, :analyzer => 'ik'
+    indexes :condition
+    indexes :start_at, :type => 'date'
+    indexes :end_at, :type => 'date'
+    indexes :status
+    indexes :est_price_low
+    indexes :est_price_high
+    indexes :realized_price
+    indexes :winner
+    indexes :loser
+    indexes :is_online
+    indexes :small_img
+    indexes :medium_img
+    indexes :big_img
+    indexes :source_url
+    indexes :topic_name
+    indexes :organizer_name
+    indexes :created_at , :type => 'date'
+    indexes :updated_at , :type => 'date'
+
+  end
+
+
+  def to_indexed_json
+    self.to_json
+  end
+
+
+end
